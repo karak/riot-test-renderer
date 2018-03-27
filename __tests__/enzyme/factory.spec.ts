1c4c403b3854e9051385bf6cb511c9c3dfc3e552
karak <karak97@gmail.com>
2018-03-27 22:50:19 +0900

    feat(enzyme): Auto detection for single tag.

--- __tests__/enzyme/factory.spec.ts
+++ __tests__/enzyme/factory.spec.ts
@@ -14,6 +14,25 @@ describe('enzyme-riot-adapter', () => {
       it('should accept 4 args', () => {
         expect(() => shallow('<tag></tag>', 'tag', { data: 'test' }, { disableLifecycleMethods: true })).not.toThrow();
       });
+
+      describe('shorthand for single tags', () => {
+        it('should accept one tag source', () => {
+          expect(() => shallow('<tag></tag>')).not.toThrow();
+        });
+
+        it('should accept one tag source beginning with spaces and line-breaks', () => {
+          expect(() => shallow(' \n\t   <tag></tag>')).not.toThrow();
+        });
+
+        it('should accept one tag source beginning with javascript expressions', () => {
+          expect(() => shallow('(function() {})();\n<tag></tag>')).not.toThrow();
+        });
+
+        it('cannot accept one tag source beginning with specific sequence as well as non-shorthand', () => {
+          expect(() => shallow('var x = "<is-not-tag>";<tag></tag>', 'tag')).toThrowError();
+          expect(() => shallow('var x = "<is-not-tag>";<tag></tag>')).toThrowError();
+        });
+      });
     });
   });
 });
--- src/enzyme/index.ts
+++ src/enzyme/index.ts
@@ -7,15 +7,42 @@ import {
 } from 'enzyme';
 
 import EnzymeRiotAdapter from './adapter';
+import isString from 'lodash/isString';
 
 configure({ adapter: new EnzymeRiotAdapter() });
+
+const SINGLE_TAG_NAME_REGEX = /^[^<]*<(.*?)>/m;
+
+/**
+ * Guess its name from single tag source
+ * without real compilation
+ *
+ * @param singleSource tag source which may be contain single tag
+ * @returns tag name
+ * @throws {Error} if no tag expression is found
+ */
+function guessNameFromSource(singleSource: string): string {
+  const m = SINGLE_TAG_NAME_REGEX.exec(singleSource);
+  if (m === null) throw new Error('No tag expression with <>');
+
+  return m[1];
+}
 
 /** Options of {@see shallow} */
 type RiotShallowRendererProps = ReactShallowRendererProps & {
   /** tag sources */
   source: string;
-}
+};
 
+/**
+ * Shallow render
+ *
+ * @param singleSource tag source which contains single tag
+ * @param opts tag interface
+ * @param options renderer options see {@see enzyme~shallow}
+ * @returns wrapper object of rendered element.
+ */
+export function shallow<TOpts>(singleSource: string, opts?: TOpts, options?: ReactShallowRendererProps): ShallowWrapper<TOpts>;
 /**
  * Shallow render
  *
@@ -25,7 +52,25 @@ type RiotShallowRendererProps = ReactShallowRendererProps & {
  * @param options renderer options see {@see enzyme~shallow}
  * @returns wrapper object of rendered element.
  */
-export function shallow<TOpts>(source: string, name: string, opts?: TOpts, options?: ReactShallowRendererProps): ShallowWrapper<TOpts> {
+export function shallow<TOpts>(source: string, name: string, opts?: TOpts, options?: ReactShallowRendererProps): ShallowWrapper<TOpts>;
+export function shallow<TOpts>(source: string, ...args: any[]): ShallowWrapper<TOpts> {
+  let name: string;
+  let opts: TOpts | undefined;
+  let options: ReactShallowRendererProps | undefined;
+
+  if (isString(args[0])) {
+    name = args[0];
+    opts = args[1];
+    // tslint:disable-next-line:no-magic-numbers
+    options = args[2];
+  } else {
+    name = guessNameFromSource(source);
+    // tslint:disable-next-line:no-magic-numbers
+    opts = args[2];
+    // tslint:disable-next-line:no-magic-numbers
+    options = args[3];
+  }
+
   const adaptee = reactShallow(
     { type: name, props: opts || {}, key: null },
     { source, ...options } as RiotShallowRendererProps,
