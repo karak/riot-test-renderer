79a9ff892ecd18e4be155ec22f6db8aa75213270
karak <karak97@gmail.com>
2018-03-28 17:00:08 +0900

    refactor(enzyme): Handle children in elementToTree

--- src/enzyme/elementToTree.ts
+++ src/enzyme/elementToTree.ts
@@ -1,20 +1,19 @@
-import { EnzymeNode } from './EnzymeNode';
+import { EnzymeElement } from './EnzymeNode';
 import { VirtualElement } from '../lib/VirtualElement';
 import map from 'lodash/map';
 import isString from 'lodash/isString';
 
-export default function elementToTree(el: VirtualElement): EnzymeNode<any> {
+export default function elementToTree(el: VirtualElement): EnzymeElement<any>;
+export default function elementToTree(el: string): string;
+export default function elementToTree(el: VirtualElement | string): EnzymeElement<any> | string {
+  if (isString(el)) return el;
+
   return {
     nodeType: 'host',
     type: el.name,
     props: el.attributes,
     key: null,
     instance: null,
-    rendered: map(el.children, x => {
-      if (isString(x)) {
-        return x;
-      }
-      return elementToTree(x);
-    }),
+    rendered: map(el.children, x => elementToTree(x)),
   };
 }
