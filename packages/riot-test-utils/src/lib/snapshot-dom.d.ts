a499c40241f43a80cbf589bd2eea6ad66324fb50
karak <karak97@gmail.com>
2018-04-11 22:54:15 +0900

    refactor: remove unused files

--- packages/riot-test-utils/src/lib/snapshot-dom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@wildpeaks/snapshot-dom' {
-  interface SnapshotElement {
-    tagName: string;
-    attributes: { [name: string]: any };
-    childNodes: SnapshotElement[];
-  }
-
-  export function toJSON(element: Element): SnapshotElement;
-}
