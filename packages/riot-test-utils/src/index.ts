3a471ecb5a8fc46a3fdacaa989b6ced4940d49b2
karak <karak97@gmail.com>
2018-04-16 13:02:43 +0900

    refactor: organize index.ts to publish

--- packages/riot-test-utils/src/index.ts
+++ packages/riot-test-utils/src/index.ts
@@ -1,3 +1,23 @@
-export * from './shallow';
-export * from './lib/mount';
+/* Riot dependency in public APIs */
+import { TagInstance, TagOpts, TagRefs, NestedTags } from 'riot';
+export {
+  TagInstance,
+  TagOpts,
+  TagRefs,
+  NestedTags,
+}
+
+/* High level APIs */
+import shallow from './lib/shallow';
+import ShallowWrapper from './lib/ShallowWrapper';
+export {
+  ShallowWrapper,
+  shallow,
+};
+export * from './lib/mount'; // !!now developing!!
+
+/* Lowlevel APIs */
+export * from './lib/renderers';
+import Simulate from './lib/Simulate';
+export { Simulate };
 export * from './lib/transform';
