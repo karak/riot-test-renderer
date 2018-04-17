5b43c78023975199c77362a88398d94bd3491950
karak <karak97@gmail.com>
2018-04-17 20:57:21 +0900

    feat(find): implement get and length.

--- packages/riot-test-utils/src/lib/wrappers/WeakWrapper.ts
+++ packages/riot-test-utils/src/lib/wrappers/WeakWrapper.ts
@@ -1,4 +1,9 @@
+import toArray from '../utils/dom/toArray';
+import lazy from '../utils/misc/lazy';
+
 export default class WeakWrapper {
+  readonly elements = lazy(() => toArray(this.nodeList));
+
   constructor(private nodeList: NodeListOf<Element>) {}
 
   get instance() {
@@ -7,6 +12,26 @@ export default class WeakWrapper {
 
   get root() {
     return this.assertSingle();
+  }
+
+  get(index: number): Element;
+  get(): ReadonlyArray<Element>;
+  get(index?: number): Element | ReadonlyArray<Element> {
+    if (index === undefined) {
+      // return all the elements
+      return this.elements();
+    } else {
+      // return each element
+      if (index < 0) {
+        // ...in reversed order
+        index = this.nodeList.length + index;
+      }
+      return this.nodeList.item(index);
+    }
+  }
+
+  get length() {
+    return this.nodeList.length;
   }
 
   private assertSingle() {
