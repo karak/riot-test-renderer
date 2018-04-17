95ad0ec2cb1da02c3706d3c024d6a3639dcc1653
karak <karak97@gmail.com>
2018-04-17 23:13:55 +0900

    fix: allow void for return type of each

--- packages/riot-test-utils/src/lib/utils/dom/each.ts
+++ packages/riot-test-utils/src/lib/utils/dom/each.ts
@@ -1,6 +1,6 @@
 export default function each<T extends Node>(
   nodes: NodeListOf<T>,
-  fn: (x: T, i: number, nodes: NodeListOf<T>) => boolean
+  fn: (x: T, i: number, nodes: NodeListOf<T>) => boolean | void
 ) {
   const n = nodes.length;
   for (let i = 0; i < n; i += 1) {
