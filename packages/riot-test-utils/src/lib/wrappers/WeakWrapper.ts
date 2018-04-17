c40874a97fcbdf8676d62244f77e8d26265a91f3
karak <karak97@gmail.com>
2018-04-17 17:55:26 +0900

    feat(find): implement instance

--- packages/riot-test-utils/src/lib/wrappers/WeakWrapper.ts
+++ packages/riot-test-utils/src/lib/wrappers/WeakWrapper.ts
@@ -1,6 +1,10 @@
 export default class WeakWrapper {
   constructor(private nodeList: NodeListOf<Element>) {}
 
+  get instance() {
+    return null;
+  }
+
   get root() {
     return this.assertSingle();
   }
