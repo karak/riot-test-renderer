aa7bea8540d4f65532e9161bbdfb3591877645ca
karak <karak97@gmail.com>
2018-04-24 15:02:12 +0900

    feat(jquery): setup new project of jquery extension

---
+++ packages/jquery/rollup.config.js
@@ -0,0 +1,27 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [{
+    file: 'dist/index.js',
+    format: 'cjs',
+  }, {
+    file: 'dist/index.mjs',
+    format: 'es2015',
+  }],
+  external: [
+    'jquery',
+    'riot',
+    'riot-test-utils',
+  ],
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          module: "es2015",
+          moduleResolution: "node",
+        },
+      },
+    }),
+  ],
+};
